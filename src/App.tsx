e4865bdedf704b21f6ea4d178e3b753fb8b54294
DomenicoSacco94 <domenico@element.in>
2025-01-23 18:26:26 +0100

    add todos

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import './App.css'
 import TextEditor from './components/TextEditor'
 import Controls from './components/Controls'
 
+//TODO create a single object for the style property inside zustand
+//TODO change the layout putting the controls as second half of the poem frame
+//TODO add image generation tool
+//TODO add background customization tool
+//TODO deploy on github pages
+
 function App() {
   return (
       <div className="app-container">
